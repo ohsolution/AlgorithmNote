bd19b9c0591f71152b6da3365aedca85d8efb4c4
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2021-02-17 06:09:01 +0900

    Create Geometry.cpp

---
+++ Math/Geometry.cpp
@@ -0,0 +1,77 @@
+struct point
+{
+	ld x, y;
+
+	bool operator >(point& b)
+	{
+		return x == b.x ? y > b.y : x > b.x;
+	}
+};
+
+struct vc
+{
+	point sp, ep;
+	ld dx, dy;
+};
+
+ld ccw(point a, point b, point c)
+{	
+	ld val = 1ll * ((b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x));	
+	if (val > 0ll) return 1; // 반시계
+	else if (val < 0ll) return -1; // 시계
+	else return 0; // 평행
+}
+
+ld ccw(vc v, vc u)
+{
+	return (v.dx * u.dy) - (v.dy * u.dx);
+}
+
+bool Isintersect(vc v, vc u)
+{
+	int ab = ccw(v.sp,v.ep,u.sp) * ccw(v.sp,v.ep,u.ep);
+	int cd = ccw(u.sp,u.ep,v.sp) * ccw(u.sp,u.ep,v.ep);
+
+	if (ab == 0 && cd == 0) // 두 벡터 평행
+	{
+		if (v.sp > v.ep) swap(v.sp, v.ep);
+		if (u.sp > u.ep) swap(u.sp, u.ep);
+		return !(u.sp > v.ep || v.sp > u.ep);
+	}
+
+	return ab <= 0 && cd <= 0;
+}
+
+vc findv(point a, point b)
+{
+	return{ a,b,b.x - a.x,b.y - a.y };
+}
+
+point geteq(vc v, vc u)
+{
+	if ((LL)ccw(v, u) == 0) // 벡터 평행 시 처리
+	{
+		if (v.sp > v.ep) swap(v.sp, v.ep);
+		if (u.sp > u.ep) swap(u.sp, u.ep);
+
+		if (v.ep.x == u.sp.x || v.ep.y == u.sp.y) return v.ep;
+		else if (u.ep.x == v.sp.x && u.ep.y == v.sp.y) return v.sp;		
+    else; // 수많은 점에서 교점
+	}
+
+	point ret = v.sp;
+
+	ld p = ccw(u, findv(v.sp, u.sp)) / ccw(v, u);
+
+	ret.x += p * (v.dx);
+	ret.y += p * (v.dy);
+
+	if (ccw(u.sp,u.ep,ret))
+	{
+		ret = v.sp;
+		ret.x -= p * (v.dx);
+		ret.y -= p * (v.dy);	
+	}
+
+	return ret;
+}

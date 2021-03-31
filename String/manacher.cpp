a20814ba7fb008d96006ac673700d18e09af1bcd
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2021-03-31 10:38:10 +0900

    Create manacher.cpp

---
+++ String/manacher.cpp
@@ -0,0 +1,33 @@
+int cir[max_v];
+
+int main()
+{
+#ifdef OHSOLUTION
+    freopen("input.txt", "r", stdin);
+#endif	
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    string t; ci(t);
+
+    string s = "#";
+    reverse(t.begin(), t.end());
+    while (t.size()) s += t.back(), s += '#', t.pop_back();
+
+    int n = s.size();
+
+    auto mana = [&]()
+    {
+        int r = 0, p = 0;
+        fa(i,0,n)
+        {
+            cir[i] = i <= r ? min(cir[2*p-i],r-(int)i): 0;
+
+            while (i - cir[i] - 1 >= 0 && i + cir[i] + 1 < n && s[i - cir[i] - 1] == s[i + cir[i] + 1]) ++cir[i];
+
+            if (r < i + cir[i]) r = i + cir[i], p = i;            
+        }
+    };
+
+    mana();    
+    fa(i, 1, n-1) co(cir[i] << " ");
+    return 0;
+}

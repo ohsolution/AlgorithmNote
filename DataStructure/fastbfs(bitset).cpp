da0a61395fb5f791a220d6373ebc3e2b3dd16dbe
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2021-05-19 12:19:08 +0900

    Update fastbfs(bitset).cpp

--- DataStructure/fastbfs(bitset).cpp
+++ DataStructure/fastbfs(bitset).cpp
@@ -1,34 +1,34 @@
 auto bfs = [&](int sp) // get longest dist from sp that can vist every vertex
 {
-		queue<int> bq;
-		bitset<max_v> cur; 
-		fa(i, 0, n) cur[i] = 1;
+	queue<int> bq;
+	bitset<max_v> cur; 
+	fa(i, 0, n) cur[i] = 1;
 
-		bq.push(sp);
-		cur[sp] = 0;
-		int cnt = 0;
+	bq.push(sp);
+	cur[sp] = 0;
+	int cnt = 0;
 
-		while (bq.size())
+	while (bq.size())
+	{
+		int sz = bq.size();
+
+		while (sz--)
 		{
-			int sz = bq.size();
+			int u = bq.front(); bq.pop();
 
-			while (sz--)
+			bitset<max_v> cbit = cur & adj[u];
+
+			if ((cur ^= cbit) == 0) return cnt+1;
+
+			while (cbit!=0)
 			{
-				int u = bq.front(); bq.pop();
-
-				bitset<max_v> cbit = cur & adj[u];
-
-				if ((cur ^= cbit) == 0) return cnt+1;
-
-				while (cbit!=0)
-				{
-					int c = cbit._Find_first();
-					cbit[c] = 0;
-					bq.push(c);
-				};
-			}						
-		}
+				int c = cbit._Find_first();
+				cbit[c] = 0;
+				bq.push(c);
+			};
+		}						
+	}
 
 
-		return -1; // impossible
+	return -1; // impossible
 };

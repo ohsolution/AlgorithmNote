6aed8895b3e256ce353d7bf623f6d66655eb73de
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2021-06-04 19:15:26 +0900

    Update extended_euc&crt.cpp

--- Math/extended_euc&crt.cpp
+++ Math/extended_euc&crt.cpp
@@ -24,36 +24,36 @@ setl get_sol(LL a, LL b , LL r)
 }
 
 auto crt = [&](LL a1, LL a2,LL m1,LL m2)
-	{		
-		// 법이 서로소가 아닐때 gcd(m1,m2) !=1
-		// x= a1(mod m1), x= a2(mod m2) <- m1x+m2y = a2-a1 
+{		
+	// 법이 서로소가 아닐때 gcd(m1,m2) !=1
+	// x= a1(mod m1), x= a2(mod m2) <- m1x+m2y = a2-a1 
 
-		if (a1 > a2) swap(m1, m2), swap(a1, a2);
-		
-		LL r = a2 - a1;
+	if (a1 > a2) swap(m1, m2), swap(a1, a2);
 
-		LL g, x, y;
-		tie(g, x, y) = egcd(m1, m2);
+	LL r = a2 - a1;
 
-		if (r % g != 0) return;
+	LL g, x, y;
+	tie(g, x, y) = egcd(m1, m2);
 
-		x *= r / g;
-		y *= r / g;
+	if (r % g != 0) return;
 
-		LL A = m2 / g;
-		LL B = m1 / g;
+	x *= r / g;
+	y *= r / g;
 
-		// x' = x + t * A 일반해 t 정수
-		// y' = y - t * B
+	LL A = m2 / g;
+	LL B = m1 / g;
 
-		if (x < 0)
-		{
-			x += A * ceil(-x / (ld)A);
-		}
+	// x' = x + t * A 일반해 t 정수
+	// y' = y - t * B
 
-		x -= A * (x / A);
+	if (x < 0)
+	{
+		x += A * ceil(-x / (ld)A);
+	}
 
-		tar.push_back(a1 + x * m1); // init crt
+	x -= A * (x / A);
 
-		return;
-	};
+	tar.push_back(a1 + x * m1); // init crt
+
+	return;
+};

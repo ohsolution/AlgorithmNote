19b48af836f86626acdc83aa9b8f058510c1ff00
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2021-09-12 09:38:41 +0900

    Update fastbfs(bitset).cpp

--- DataStructure/fastbfs(bitset).cpp
+++ DataStructure/fastbfs(bitset).cpp
@@ -36,3 +36,37 @@ auto bfs = [&](int sp) // get longest dist from sp that can vist every vertex
 
 	return -1; // impossible
 };
+
+auto dijk = [&](int sp)
+	{
+		priority_queue<setl, vector<setl>, lcmp> pq;
+		bitset<max_v> cur;
+		fa(i, 0, n) cur[i] = 1;
+
+		pq.push({ sp,0 });
+		dist[sp][sp] = 0;
+		cur[sp] = 0;
+		
+		while (pq.size())
+		{
+			auto [u, d] = pq.top(); pq.pop();
+
+			if (d ^ dist[sp][u]) continue;
+
+			bitset<max_v> cbit = cur & adj[u];
+
+			while (cbit != 0)
+			{
+				int c = cbit._Find_first();
+				cbit[c] = 0;
+				cur[c] = 0;
+				if (dist[sp][c] > dist[sp][u] + cost[u][c])
+				{
+					dist[sp][c] = dist[sp][u] + cost[u][c];
+					pq.push({ c,dist[sp][c] });
+				}
+			};
+		}
+		return;
+	};
+
